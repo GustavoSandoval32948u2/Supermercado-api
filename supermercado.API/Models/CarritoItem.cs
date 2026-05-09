88dbd3d7d405ca36ddec86df7d5fa36fe48139a1
EdinAdolfo <emartinezs19@miumg.edu.gt>
2026-05-08 18:05:43 -0600

    Agregada API E-Commerce

---
+++ supermercado.API/Models/CarritoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace supermercado.API.Models
+{
+    public class CarritoItem
+    {
+        public int ProductoId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal Precio { get; set; }
+    }
+}
